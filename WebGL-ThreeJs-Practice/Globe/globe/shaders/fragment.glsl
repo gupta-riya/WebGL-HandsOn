0cdf711072b2653ab01e23088b817b0fd456cbea
gupta-riya <reeya211999@gmail.com>
2021-06-04 02:22:24 +0530

    added atmospheric glow

--- WebGL-ThreeJs-Practice/Globe/globe/shaders/fragment.glsl
+++ WebGL-ThreeJs-Practice/Globe/globe/shaders/fragment.glsl
@@ -2,8 +2,16 @@
 uniform sampler2D globeTexture;
 
 varying vec2 vertexUV;  // eg: [0,0.24]
+varying vec3 vertexNormal;
+
 void main(){
+    // atmospheric glow for every pixel
+    float intensity = 1.05 - dot(vertexNormal, vec3(0.0, 0.0,1.0));
+    
+                    // shade of color of atmosphere
+    vec3 atmosphere = vec3(0.3,0.6,1.0)*pow(intensity,1.5);
     // param -> samplerObj, uv coord
     // texture2D(globeTexture,vertexUV)
-    gl_FragColor = texture2D(globeTexture,vertexUV);
+    // adding atmosphere is creating a great effect
+    gl_FragColor = vec4(atmosphere + texture2D(globeTexture,vertexUV).xyz, 1.0);
 }
--- WebGL-ThreeJs-Practice/Globe/globe/shaders/vertex.glsl
+++ WebGL-ThreeJs-Practice/Globe/globe/shaders/vertex.glsl
@@ -1,11 +1,13 @@
 // glsl is a typed language , we need to define return type and all
 
 varying vec2 vertexUV;
+varying vec3 vertexNormal;
 
 // we our creating our own mesh material
 void main(){
     vertexUV = uv;  // uv is provided by threejs by default
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1);    // vec4 same as (1,0,0,1)
+    vertexNormal = normal;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);    // vec4 same as (1,0,0,1)
 }
 
 
