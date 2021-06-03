b71ae05b7392ddf72918942d3ca7efa23d0f91b3
gupta-riya <reeya211999@gmail.com>
2021-06-04 03:03:32 +0530

    added background stars

--- WebGL-ThreeJs-Practice/Globe/globe/main.js
+++ WebGL-ThreeJs-Practice/Globe/globe/main.js
@@ -1,6 +1,10 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js'
 import vertexShader from './shaders/vertex.glsl';
 import fragmentShader from './shaders/fragment.glsl';
+import atmosphereVertexShader from './shaders/atmosphereVertex.glsl';
+import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl';
+import gsap from 'gsap';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -16,7 +20,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 
 document.body.appendChild(renderer.domElement);
 
-// create sphere
+// create first sphere
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5,50,50),  //SphereGemoetry param -> radius, widthSegments, heightSegments 
   new THREE.ShaderMaterial({
@@ -32,13 +36,83 @@ const sphere = new THREE.Mesh(
 
 scene.add(sphere)
 
+// create second sphere
+const atmosphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5.5,50,50),  //SphereGemoetry param -> radius, widthSegments, heightSegments 
+  new THREE.ShaderMaterial({
+    vertexShader: atmosphereVertexShader, // same as vertexShader: vertexShader
+    fragmentShader: atmosphereFragmentShader,
+    blending: THREE.AdditiveBlending,
+    side: THREE.BackSide
+  })
+  ) 
+atmosphere.scale.set(1.1,1.1,1.1)
+scene.add(atmosphere)
+
+const group = new THREE.Group()
+group.add(sphere)
+scene.add(group)
+
+// stars
+const starGeometry = new THREE.BufferGeometry()
+const starMaterial = new THREE.PointsMaterial({
+  color: 0xffffff
+})
+
+const starVertices = []
+for(let i = 0 ; i < 10000; i++)
+{
+  const x = (Math.random() - 0.5)*2000
+  const y = (Math.random() - 0.5)*2000
+  const z = -(Math.random())*3000      // we only want stars at back
+  starVertices.push(x,y,z)
+
+}
+
+starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(
+  starVertices,3))
+
+const stars = new THREE.Points(
+  starGeometry,starMaterial)
+
+scene.add(stars)
+
+
+
+
 camera.position.z = 15;
 
+
+const mouse = {
+  x : undefined,
+  y : undefined
+}
+
+// add mouse hover effect
+
+addEventListener('mousemove',(event)=>{
+
+  // to get center coordinate as 0 and if we move left we want to have coordinate in neg so we applied a formula 
+  mouse.x = (event.clientX / innerWidth)*2 - 1;
+  // reverse case in y (coz we want -ve in bottom)
+  mouse.y = -(event.clientY / innerHeight)*2 + 1;
+
+
+})
 
 function animate()
 {
     requestAnimationFrame(animate)
     renderer.render(scene,camera)
+    sphere.rotation.y += 0.002
+    // group.rotation.x = mouse.x * 0.5
+    gsap.to(group.rotation,{
+      x:- mouse.y * 0.3,
+      y: mouse.x * 0.5,
+      duration:2
+    })
+
 }
 
 animate()
+
--- WebGL-ThreeJs-Practice/Globe/globe/shaders/vertex.glsl
+++ WebGL-ThreeJs-Practice/Globe/globe/shaders/vertex.glsl
@@ -6,7 +6,7 @@ varying vec3 vertexNormal;
 // we our creating our own mesh material
 void main(){
     vertexUV = uv;  // uv is provided by threejs by default
-    vertexNormal = normal;
+    vertexNormal =  normalize(normalMatrix*normal);
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);    // vec4 same as (1,0,0,1)
 }
 
